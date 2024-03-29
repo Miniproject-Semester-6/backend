cb6db23e0fd77c4372b0fd0ff7417937367818cb
chinmay-gupta-2003 <guptachinmay2003@gmail.com>
2024-03-29 23:30:27 +0530

    connect to db

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
 const app = require("./app");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    process.env.NODE_ENV === "production"
+      ? process.env.DATABASE_HOSTED
+      : process.env.DATABASE_LOCAL,
+  )
+  .then(() => {
+    console.log("Database connected successfully");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
