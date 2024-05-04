2f0655585cd0167efe18b6ab6e1d8e519b552f58
chinmay-gupta-2003 <guptachinmay2003@gmail.com>
2024-05-04 16:46:12 +0530

    Add date range filter to getAllExpenses controller

--- controllers/expense/getAllExpenses.js
+++ controllers/expense/getAllExpenses.js
@@ -4,11 +4,19 @@ const Expense = require("../../models/expense");
 const getAllExpense = async (req, res, next) => {
   try {
     const query = {};
+    const { startDate, endDate } = req.query;
 
     if (!req.query.budgetid && req.user.role !== "SUPER_ADMIN")
       return next(new AppError("You are not authorised for this action!", 403));
 
     if (req.query.budgetid) query.budget = req.query.budgetid;
+
+    if (startDate && endDate) {
+      query.createdAt = {
+        $gte: new Date(startDate),
+        $lte: new Date(endDate),
+      };
+    }
 
     const expenses = await Expense.find(query);
     const budget = await Budget.findById(req.query.budgetid);
